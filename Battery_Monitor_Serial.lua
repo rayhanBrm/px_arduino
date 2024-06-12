3a5304bd339f1543ec6297349d65d484053132eb
rayhanBrm <131897565+rayhanBrm@users.noreply.github.com>
2024-06-13 03:13:04 +0700

    Update Battery_Monitor_Serial.lua

--- Battery_Monitor_Serial.lua
+++ Battery_Monitor_Serial.lua
@@ -1,16 +1,16 @@
 ---@diagnostic disable: need-check-nil
 
-local port = serial:find_serial(0)
+local port = serial:find_serial(0) -- find_serial(0) = port TELEM2, find_serial(1) = port GPS2
 
 if not port then
-    gcs:send_text(0, "PORT SALAH")
+    gcs:send_text(0, "PORT SALAH") -- jika port tidak terhubung dengan benar maka masuk kondisi ini
     return
 end
 
-port:begin(57600)
+port:begin(57600) -- baudrate harus sama antara pixhawk dan arduino
 port:set_flow_control(0)
 
-function update()
+function update() -- fungsi untuk update kondisi yang akan dipanggil berkali kali untuk mengupdate data batre yang dikirim dari arduino
     if port:available() then
         local data = ""
         local byte = port:read()
@@ -26,7 +26,7 @@ function update()
         -- gcs:send_text(0, "Data received: " .. data)  -- Debugging information
 
         if #data > 0 then
-            local voltage_str = data:match("%d+%.%d+")
+            local voltage_str = data:match("%d+%.%d+") -- matching data bentuk float di arduino untuk diterima
 
             if voltage_str then  -- Ensure voltage_str is not nil
                 local voltage = tonumber(voltage_str)
@@ -40,7 +40,7 @@ function update()
                 gcs:send_text(0, "No voltage string found")
             end
         --else
-          --  gcs:send_text(0, "TIDAK ADA DATA")
+          --  gcs:send_text(0, "TIDAK ADA DATA") --digunakan untuk debugging
         end    
     else
         gcs:send_text(0, "No data available from port")
